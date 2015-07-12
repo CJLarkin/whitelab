c05858ffa06eaeb01c49725ee9b7ace07a86751f
Collin Larkin <collinjlarkin@Collins-MacBook-Pro.local>
2015-07-12 01:53:48 -0400

    updates regarding database interaction

--- Tornado/database.py
+++ Tornado/database.py
@@ -6,12 +6,13 @@ def db_edit(smiles, tt, tm, vis, frag, cit, Abs):
 
 	#conn.execute("CREATE TABLE GLC(SMILES, TT INT, TM INT, Viscocity INT, Fragility INT, Citation INT, Abstract TEXT)")
 	statement = "INSERT INTO GLC (SMILES, TT, TM, Viscocity, Fragility, Citation, Abstract) VALUES ('{0}',{1},{2},{3},{4},{5},'{6}')".format(smiles, tt, tm, vis, frag, cit, Abs)
-	#conn.execute("INSERT INTO GLC (SMILES, TT, TM, Viscocity, Fragility, Citation, Abstract) VALUES ({0},{1},{2},{3},{4},{5},{6})".format(smiles, tt, tm, vis, frag, cit, Abs))
-	print statement
+	#print statement
 	conn.execute(statement)
 	thing = conn.execute("SELECT SMILES, TT, TM, Viscocity, Fragility, Citation, Abstract from GLC")
 	for row in thing:
 		return row[0],row[1],row[2],row[3],row[4],row[5],row[6]
+	#FOR SEARCHING
+	cur.execute("SELECT * FROM list WHERE InstitutionName=?", (Variable,))
 	conn.commit()
 	conn.close()
 
