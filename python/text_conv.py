717a05dccacd8e1c8c980f6b9be6853047bb5183
Collin Larkin <collinjlarkin@dhcp-10-5-8-134.wireless.rochester.edu>
2018-02-27 11:05:55 -0500

    first update

--- python/text_conv.py
+++
@@ -1,9 +0,0 @@
-import 
-import sys
-def text_conv(image):
-	img_recovered = base64.decodestring(image)
-	fname = ''.join(random.choice(string.ascii_lowercase + string.digits) for x in range(6))
-	f = open("uploads/" + fname, 'w')
-	f.write(img_recovered)
-	f.close()
-text_conv(sys.argv[1])
